2004986f725dad981a5c7269df9c1c7bc77ceed3
huang.feixian <xianandzai@gmail.com>
2019-01-02 17:33:12 +0800

    delete md5

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019.1.2
+ * Time: 16:15
+ */
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    static public function getArticles()
+    {
+        
+    }
+}
